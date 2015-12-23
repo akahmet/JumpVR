e495b6f331e4f04287d8e346e2b6b096d86be99c
akahmet <ahmet.ak@stu.bahcesehir.edu.tr>
2015-12-23 03:37:13 +0200

    Create JumpVR.js

---
+++ JumpVR.js
@@ -0,0 +1,66 @@
+/* globals THREE */
+/**
+ * JumpVR - Sensing Jump with Device acceleration
+ *
+ * @param {Object, Object}
+ * @constructor
+ *
+ * @author Ahmet AK / http://blog.mechatronian.com/
+ *
+ * W3C Device Orientation event
+ * (http://www.w3.org/TR/orientation-event/)
+ */
+
+ 
+ 
+(function() {
+		
+	THREE.JumpVR = function(event,options) {
+		this.options = options || {max: 3, min: -3/*g*/, TimeOut: 500/* ms */, minTime: 10/* ms */ }
+		this.FeedBack = event;
+		this.count = 0;
+		this.TimeOut = 0;
+		this.Up = false; //
+		
+		this.Millis=function() {
+			return new Date().getTime();
+		};
+		
+		this.onOrientation = function (e){
+			console.log(e);
+		}
+		
+		this.onMotion = function (e){
+			var Ax=e.acceleration.x;
+			var Ay=e.acceleration.y;
+			var Az=e.acceleration.z;
+			
+			var result = false;
+			var calc=Ax*Math.sin() + Az * Math.cos() + Ay * Math.cos();
+			
+			if (calc > this.options.max){
+				this.Up = true;
+				this.TimeOut = this.Millis();
+				//this.count++;
+			}
+						
+			if ((this.Millis()-this.TimeOut) > this.options.TimeOut){
+				this.Up = false;
+			}
+			
+			if (this.Up == true && calc < this.options.min && (this.Millis()-this.TimeOut) > this.options.minTime){
+				this.count++;
+				this.Up = false;
+				result = true;
+			}
+			
+			
+			this.FeedBack({result: result,count: this.count,event: e});
+		}.bind(this);
+		
+		window.addEventListener('devicemotion', this.onMotion, false);
+		//window.addEventListener('deviceorientation', this.onOrientation, false);
+
+	}
+})();
+
